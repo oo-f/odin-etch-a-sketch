354f8127d2675a8ea6d12b5fcf37adcfe9a41b37
oo-f <179203317+oo-f@users.noreply.github.com>
2024-10-14 17:35:55 -0700

    Add script.js and style.css
    Added a container to house the sketchpad
    Added the logic for the sketchpad
    Added two buttons (for clearing and setting size)

---
+++ script.js
@@ -0,0 +1,50 @@
+const chooseSize = document.getElementById("chooseSize");
+const clearAll = document.getElementById("clear");
+const main = document.getElementById("main");
+const containerSize = 400;
+
+    
+const createGrid = (rows, cols) => {
+    main.innerHTML = "";
+
+    const cellSize = (containerSize - (rows - 1) * 0) / rows;
+
+    for (let row = 0; row < rows; row++) {
+      const rowDiv = document.createElement("div");
+      rowDiv.className = "row";
+
+      for (let col = 0; col < cols; col++) {
+        const div = document.createElement("div");
+        div.className = "cell";
+        div.style.width = `${cellSize}px`;
+        div.style.height = `${cellSize}px`;
+
+
+        div.addEventListener("mouseover", function() {
+        div.style.backgroundColor = "#000";
+      });   
+
+        rowDiv.appendChild(div); // Append the column div to the row
+      }
+
+      main.appendChild(rowDiv);
+    }
+}
+
+createGrid(100, 100);
+
+chooseSize.addEventListener("click", function() {
+  const rows = prompt("Amount of Rows");
+  const cols = prompt("Amount of Columns");
+
+  if (rows && cols) {
+    createGrid(Number(rows), Number(cols));
+  }
+});  
+
+  clearAll.addEventListener("click", function() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => { 
+      cell.style.backgroundColor = "#fff";
+   });
+});
